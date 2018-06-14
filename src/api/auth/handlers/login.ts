0efa011ead9c2fc8ae363770c118af1bca35970f
Bogdan Mackiewicz <bogdan@mackiewicz.in>
2018-06-14 14:53:02 +0200

    fixed function to generate token

--- src/api/auth/handlers/login.ts
+++ src/api/auth/handlers/login.ts
@@ -1,5 +1,6 @@
 import { Request, ResponseToolkit } from 'hapi';
+import { createToken } from '../../../auth/helpers/jwt-create-token';
 
-export async function testHandler(_request: Request, _h: ResponseToolkit): Promise<object> {
-    return {test: 'test'};
+export async function testHandler(_request: Request, _h: ResponseToolkit): Promise<string> {
+    return createToken('nowak', 'user');
 }

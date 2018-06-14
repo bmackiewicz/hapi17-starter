1cc9ef02ee5876cdebf342e68f19e4b02e37ba67
Bogdan Mackiewicz <bogdan@mackiewicz.in>
2018-06-14 14:09:51 +0200

    add typeOrm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import * as Api from './server';
 import { Server } from 'hapi';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { get } from "config";
 
 async function start(): Promise<void> {
     try {
         const server: Server = await Api.init();
+        await createConnection({
+            name: 'default',
+            type: 'mongodb',
+            host: get<string>('typeorm.mongodb.host'),
+            port: get<number>('typeorm.mongodb.port'),
+            username: get<string>('typeorm.mongodb.username'),
+            password: get<string>('typeorm.mongodb.password'),
+            database: get<string>('typeorm.mongodb.database'),
+            authSource: get<string>('typeorm.mongodb.authSource'),
+            ssl: get<boolean>('typeorm.mongodb.ssl'),
+            sslValidate: get<boolean>('typeorm.mongodb.sslValidate'),
+            entities: [ __dirname + '/entity/{*.ts,*.js}'],
+            synchronize: get<boolean>('typeorm.mongodb.synchronize'),
+            logging: 'all',
+          }).catch(error => console.error(error));
         await server.start();
-        console.log('Server running at:', server.info.uri);
+        server.log('Server running at:', server.info.uri);
     } catch (err) {
         console.error('Error starting server: ', err.message);
         throw err;
